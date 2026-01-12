34b31cab5636bb99547ca04028d95d3f21489373
william-francia <wfranciapoma@byupathway.edu>
2026-01-12 17:40:51 -0400

    this the the first Exercice

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.Write($"Your name is {name}, {lastname} {name}.");
     }
 }
