02fe2eddb9ac89c9a5489760d64a3641975fdbd7
william-francia <wfranciapoma@byupathway.edu>
2026-01-12 21:26:15 -0400

    this is the second file

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation, keep doing like this and better, you can give more like this.");
+        }
+        else
+        {
+            Console.WriteLine("We appologize, you have to repit the course.");
+        }
     }
 }
