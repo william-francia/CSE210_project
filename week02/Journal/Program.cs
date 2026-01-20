c34f562f8e1d2cfcddf5cc1343e2edf57f77d5d1
william-francia <wfranciapoma@byupathway.edu>
2026-01-20 12:27:01 -0400

    Changes

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Data;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        int Response = 0;
+
+        while (Response == 5)
+        {
+            Console.WriteLine("Welcome to the Jornal program!");
+            Console.WriteLine("Please select one of the following choices");
+
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.WriteLine("What would you like to do!");
+            Response = int.Parse(Console.ReadLine());
+
+
+
+            switch (Response)
+            {
+                case 1:// for Write
+                break;
+
+                case 2:// for Display
+                break;
+
+                case 3:// for Load
+                break;
+
+                case 4:// for Save
+                break;
+
+                case 5:// for Quit
+                    Console.WriteLine("See you tomorrow!");
+                break;
+            
+
+                default:
+                    Console.WriteLine("Chosse another number from 1 to 5!");
+                break;
+            }
+
+        }
+
+
     }
+
+
 }
